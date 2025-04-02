2b55bbd488e52535f8c4a64b6bfd6f35372da40b
Mizuba74 <kevinleal@hotmail.com>
2025-04-02 16:04:51 +0200

    add: StatusPage components

--- frontend/applitrack/src/pages/Temp.tsx
+++ frontend/applitrack/src/pages/Temp.tsx
@@ -1,6 +1,4 @@
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { DataTable } from "@/components/data-table";
-import { SectionCards } from "@/components/section-cards";
 import useApplications from "@/hooks/useApplications";
 
 export function Temp() {
@@ -19,11 +17,9 @@ export function Temp() {
 		<div className="flex flex-1 flex-col">
 			<div className="@container/main flex flex-1 flex-col gap-2">
 				<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-					<SectionCards />
 					<div className="px-4 lg:px-6">
-						<ChartAreaInteractive />
+						<DataTable data={data} />
 					</div>
-					<DataTable data={data} />
 				</div>
 			</div>
 		</div>
