90e6e4ee053a3cbe90b991e9c8483a69da52324a
Mizuba74 <kevinleal@hotmail.com>
2025-04-16 17:13:54 +0200

    fix docker hope last time

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,6 @@ RUN go mod download
 
 COPY . .
 COPY ./migrations /app/migrations
-COPY ./migrations/tern.conf ./tern.conf
 
 RUN go build -o main main.go
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo " Running DB migrations with Tern..."
-tern migrate 
+tern migrate -c /app/migrations/tern.conf -d /app/migrations || exit 1
 
 echo " Starting API server..."
 ./main
