46958f9da5320f849b56d92bdea2809928b2e5da
Mizuba74 <kevinleal@hotmail.com>
2025-04-16 13:25:40 +0200

    fix address

--- internal/server/server.go
+++ internal/server/server.go
@@ -10,7 +10,7 @@ func InitServer() {
 	router := Router()
 	fmt.Printf("Sucessfull connection to server on 8080")
 
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	if err := http.ListenAndServe("0.0.0.0:8080", router); err != nil {
 		fmt.Printf("Error connection to server: %v", err)
 	}
 
